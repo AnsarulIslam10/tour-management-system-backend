83f671c379186a3a2bcdb81067082bc752a95735
AnsarulIslam10 <ansarulislam01739@gmail.com>
2025-07-11 23:56:51 +0600

    error helper

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -2,9 +2,11 @@
 import { NextFunction, Request, Response } from "express";
 import httpStatus from 'http-status-codes';
 import { UserServices } from "./user.service";
+import AppError from "../../errorHelpers/AppError";
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
     try {
+        // throw new AppError(httpStatus.BAD_REQUEST, "fale error")
         const user = await UserServices.createUser(req.body)
         
         res.status(httpStatus.CREATED).json({
