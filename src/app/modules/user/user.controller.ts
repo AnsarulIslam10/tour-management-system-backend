dfd69313e67a22d8351089e545a91c1ca0d96f1a
AnsarulIslam10 <ansarulislam01739@gmail.com>
2025-07-12 11:54:05 +0600

    send response utility function

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -4,37 +4,28 @@ import { NextFunction, Request, Response } from "express";
 import httpStatus from 'http-status-codes';
 import { UserServices } from "./user.service";
 import { catchAsync } from "../../utils/catchAsync";
+import { sendResponse } from "../../utils/sendResponse";
 
-
-
-// const createUser = async (req: Request, res: Response, next: NextFunction) => {
-//     try {
-//         // throw new AppError(httpStatus.BAD_REQUEST, "fale error")
-//         const user = await UserServices.createUser(req.body)
-
-//         res.status(httpStatus.CREATED).json({
-//             message: "User Created Successfully"
-//         })
-//     } catch (err: any) {
-//         console.log(err)
-//         next(err)
-//     }
-// }
 const createUser = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
     const user = await UserServices.createUser(req.body)
 
-    res.status(httpStatus.CREATED).json({
+    sendResponse(res, {
+        success: true,
+        statusCode: httpStatus.CREATED,
         message: "User Created Successfully",
-        user
+        data: user,
+
     })
 })
 
 const getAllUsers = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
-    const users = await UserServices.getAllUsers()
-    res.status(httpStatus.OK).json({
+    const result = await UserServices.getAllUsers()
+    sendResponse(res, {
         success: true,
-        message: "All Users Retrieved Successfully",
-        data: users
+        statusCode: httpStatus.CREATED,
+        message: "All Users Retrieced Successfully",
+        data: result.data,
+        meta: result.meta,
     })
 })
 
