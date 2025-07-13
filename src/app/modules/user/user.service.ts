4105e5de628e2c6b55632dfba9d23a31244aff3a
AnsarulIslam10 <ansarulislam01739@gmail.com>
2025-07-13 11:00:13 +0600

    hashing password

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -3,20 +3,25 @@ import httpStatus from 'http-status-codes';
 import AppError from "../../errorHelpers/AppError";
 import { IAuthProvider, IUser } from "./user.interface";
 import { User } from "./user.model";
+import bcriptjs from 'bcryptjs'
 
 const createUser = async (payload: Partial<IUser>) => {
-    const { email, ...rest } = payload;
+    const { email, password, ...rest } = payload;
 
     const IsUserExist = await User.findOne({email})
     if (IsUserExist) {
         throw new AppError(httpStatus.BAD_REQUEST, "User Already Exist")
     }
 
+    const hashedPassword = await bcriptjs.hash(password as string, 10)
+
+
     const authProvider: IAuthProvider = {provider: "credentials", providerId: email as string}
 
 
     const user = await User.create({
         email,
+        password: hashedPassword,
         auths: [authProvider],
         ...rest
     })
