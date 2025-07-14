4c7fb5e00efd2e44daddc925d30c1080469357c4
AnsarulIslam10 <ansarulislam01739@gmail.com>
2025-07-14 20:59:02 +0600

    refresh token

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -4,9 +4,20 @@ import { NextFunction, Request, Response } from "express"
 import { catchAsync } from "../../utils/catchAsync"
 import { sendResponse } from "../../utils/sendResponse"
 import { AuthServices } from './auth.service';
+import AppError from '../../errorHelpers/AppError';
 
 const credentialsLogin = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
     const loginInfo = await AuthServices.credentialsLogin(req.body)
+
+    res.cookie("accessToken", loginInfo.accessToken, {
+        httpOnly: true,
+        secure: false
+    })
+
+    res.cookie("refreshToken", loginInfo.refreshToken, {
+        httpOnly: true,
+        secure: false,
+    })
     sendResponse(res, {
         success: true,
         statusCode: httpStatus.OK,
@@ -15,7 +26,22 @@ const credentialsLogin = catchAsync(async (req: Request, res: Response, next: Ne
     })
 })
 
+const getNewAccessToken = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    const refreshToken = req.cookies.refreshToken;
+    if (!refreshToken) {
+        throw new AppError(httpStatus.BAD_REQUEST, "No refresh token recieved from cookies")
+    }
+    const tokenInfo = await AuthServices.getNewAccessToken(refreshToken)
+    sendResponse(res, {
+        success: true,
+        statusCode: httpStatus.OK,
+        message: "User Logged In Successfully",
+        data: tokenInfo
+    })
+})
+
 
 export const AuthControllers = {
-    credentialsLogin
+    credentialsLogin,
+    getNewAccessToken
 }
--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -2,10 +2,12 @@
 import bcryptjs from "bcryptjs";
 import httpStatus from 'http-status-codes';
 import AppError from "../../errorHelpers/AppError";
-import { IUser } from "../user/user.interface";
+import { createUserTokens } from "../../utils/userTokens";
+import { IsActive, IUser } from "../user/user.interface";
 import { User } from '../user/user.model';
-import { generateToken } from "../../utils/jwt";
+import { generateToken, verifyToken } from "../../utils/jwt";
 import { envVars } from "../../config/env";
+import { JwtPayload } from 'jsonwebtoken';
 const credentialsLogin = async (payload: Partial<IUser>) => {
     const { email, password } = payload;
     const isUserExist = await User.findOne({ email })
@@ -19,25 +21,46 @@ const credentialsLogin = async (payload: Partial<IUser>) => {
         throw new AppError(httpStatus.BAD_REQUEST, "Incorrect Password")
     }
 
-    const jwtPayload = {
+    const userTokens = createUserTokens(isUserExist)
+
+    // delete isUserExist.password;
+    const {password: pass, ...rest} = isUserExist.toObject()
+
+    return {
+        accessToken: userTokens.accessToken,
+        refreshToken: userTokens.refreshToken,
+        user: rest
+    }
+}
+const getNewAccessToken = async (refreshToken: string) => {
+    const verifiedRefreshToken = verifyToken(refreshToken, envVars.JWT_REFRESH_SECRET) as JwtPayload
+
+
+    const isUserExist = await User.findOne({ email: verifiedRefreshToken.email })
+    if (!isUserExist) {
+        throw new AppError(httpStatus.BAD_REQUEST, "User does not exist")
+    }
+    if (isUserExist.isActive === IsActive.BLOCKED || isUserExist.isActive === IsActive.INACTIVE) {
+        throw new AppError(httpStatus.BAD_REQUEST, `User is ${isUserExist.isActive}`)
+    }
+    if (isUserExist.isDeleted) {
+        throw new AppError(httpStatus.BAD_REQUEST, "User is Deleted")
+    }
+
+    const JwtPayload={
         userId: isUserExist._id,
         email: isUserExist.email,
         role: isUserExist.role
     }
-    const accessToken = generateToken(jwtPayload, envVars.JWT_ACCESS_SECRET, envVars.JWT_ACCESS_EXPIRES)
+    const accessToken = generateToken(JwtPayload, envVars.JWT_ACCESS_SECRET, envVars.JWT_ACCESS_EXPIRES)
 
-    const refreshToken = generateToken(jwtPayload, envVars.JWT_REFRESH_SECRET, envVars.JWT_REFRESH_EXPIRES)
-
-    // delete isUserExist.password;
-    const {password: pass, ...rest} = isUserExist
 
     return {
-        accessToken,
-        refreshToken,
-        user: rest
+        accessToken
     }
 }
 
 export const AuthServices = {
-    credentialsLogin
+    credentialsLogin,
+    getNewAccessToken
 }
