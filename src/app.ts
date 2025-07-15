48bf2d7ecb2490971ee7851d2cfa4acea4ee3859
AnsarulIslam10 <ansarulislam01739@gmail.com>
2025-07-15 20:37:03 +0600

    passport js setup and google auth configuration

--- src/app.ts
+++ src/app.ts
@@ -6,8 +6,19 @@ import { globalErrorHandler } from './app/middlewares/golbalErrorHandler';
 import notFound from './app/middlewares/notFound';
 import { router } from './app/routes';
 import cookieParser from 'cookie-parser'
-
+import passport from 'passport';
+import expressSession from 'express-session'
 const app = express()
+
+app.use(expressSession({
+    secret: "Your Secret",
+    resave: false,
+    saveUninitialized: false
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
+
 app.use(cookieParser())
 app.use(express.json())
 app.use(cors())
