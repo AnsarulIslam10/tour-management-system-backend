850ef69b0c1a01d4955b8b1ce44eabe8dedfcf8e
AnsarulIslam10 <ansarulislam01739@gmail.com>
2025-07-17 08:53:36 +0600

    validation error handling

--- src/app/middlewares/golbalErrorHandler.ts
+++ src/app/middlewares/golbalErrorHandler.ts
@@ -3,14 +3,35 @@
 import { NextFunction, Request, Response } from "express"
 import { envVars } from "../config/env"
 import AppError from "../errorHelpers/AppError"
+import { TErrorSources } from "../interfaces/error.types"
+import { handlerValidationError } from "../helpers/handlerValidationError"
 
 
 export const globalErrorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
 
+    let errorSources: TErrorSources[] = []
     let statusCode = 500
     let message = "Something Went Wrong!!"
-
-    if (err instanceof AppError) {
+    //Duplicate error
+    if (err.code === 11000) {
+        console.log("Duplicate error", err.message)
+        const duplicate = err.message.match(/"([^""]*)"/)
+        statusCode = 400
+        message = `${duplicate[1]} already exist`
+    }
+    // Object ID error / Cast Error
+    else if (err.name === "CastError") {
+        statusCode = 400;
+        message = "Invalid MongoDb ObjectID, Please provide a valid ID";
+    }
+    //Mongoose Validation Error
+    else if (err.name === "ValidationError") {
+        const simplifiedError = handlerValidationError(err)
+        statusCode = simplifiedError.statusCode;
+        errorSources = simplifiedError.errorSources as TErrorSources[]
+        message = simplifiedError.message
+    }
+    else if (err instanceof AppError) {
         statusCode = err.statusCode
         message = err.message
     } else if (err instanceof Error) {
