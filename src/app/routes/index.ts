8d01d298c4717ef793351e1178078b29fd0e8225
AnsarulIslam10 <ansarulislam01739@gmail.com>
2025-07-22 18:29:01 +0600

    payment system intigration with ssl commerz

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -4,6 +4,7 @@ import { AuthRoutes } from "../modules/auth/auth.route"
 import { DivisionRoutes } from "../modules/division/division.route"
 import { TourRoutes } from "../modules/tour/tour.route"
 import { BookingRoutes } from "../modules/booking/booking.route"
+import { PaymentRoutes } from "../modules/payment/payment.route"
 
 export const router = Router()
 const moduleRoutes = [
@@ -26,9 +27,13 @@ const moduleRoutes = [
     {
         path: "/booking",
         route: BookingRoutes
+    },
+    {
+        path: "/payment",
+        route: PaymentRoutes
     }
 ]
 
-moduleRoutes.forEach((route) =>{
+moduleRoutes.forEach((route) => {
     router.use(route.path, route.route)
 })
