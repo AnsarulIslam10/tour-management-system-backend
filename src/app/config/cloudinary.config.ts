65b5f9ab8dac29128f57ab073bc142632394e451
AnsarulIslam10 <ansarulislam01739@gmail.com>
2025-07-23 14:07:18 +0600

    image delete on error

--- src/app/config/cloudinary.config.ts
+++ src/app/config/cloudinary.config.ts
@@ -1,5 +1,7 @@
-import { v2 as cloudinary} from "cloudinary";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { v2 as cloudinary } from "cloudinary";
 import { envVars } from "./env";
+import AppError from "../errorHelpers/AppError";
 
 cloudinary.config({
     cloud_name: envVars.CLOUDINARY.CLOUDINARY_CLOUD_NAME,
@@ -8,4 +10,25 @@ cloudinary.config({
 })
 
 // const uploadToCloudinary = cloudinary.uploader.upload()
+
+export const deleteImageFromCloudinary = async (url: string) => {
+    try {
+        const regex = /\/v\d+\/(.*?)\.(jpg|jpeg|png|gif|webp)$/i;
+
+        const match = url.match(regex);
+
+        console.log({ match });
+
+        if (match && match[1]) {
+            const public_id = match[1];
+            await cloudinary.uploader.destroy(public_id)
+            console.log(`File ${public_id} is deleted from cloudinary`);
+
+        }
+    } catch (error: any) {
+        throw new AppError(401, "Cloudinary image deletion failed", error.message)
+    }
+}
+
 export const cloudinaryUpload = cloudinary
+
